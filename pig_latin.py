6a7b41065face5839ca39727023a73d3475f8a64
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-03-31 17:50:45 +0000

    pigWord

--- pig_latin.py
+++ pig_latin.py
@@ -13,7 +13,28 @@ def is_vowel(char):
 		>>> is_vowel(y)
 		False
 	"""
-	return "aeioiu".find(char) != -1
+	return "aeioiuy".find(char.lower()) != -1
+	
+def is_upper(word):
+	return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.find(word[0]) != -1
+
+def first_sound(word):
+	if word[0] == 'y':
+		return 'y'
+	index = 0
+	while not is_vowel(word[index]):
+		index += 1
+	return word[:index]
+
+def pigWord(word):
+	if first_sound(word) == '':
+		sound = 'w'
+	else:
+		sound = first_sound(word)
+	output = word[len(first_sound(word)):] + sound + 'ay'
+	if is_upper(word):
+		output = output.capitalize()
+	return output
 
 if __name__ == "__main__":
 	import doctest
