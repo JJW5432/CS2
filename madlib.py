dd6f76a38d54ba9c275d169ff0e2d4d6eb8b1638
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-04-28 20:00:58 -0400

    tests in file

--- madlib.py
+++ madlib.py
@@ -43,3 +43,19 @@ def fillBlanks(story):
         word = choice(word_list)
         story = indexReplace(tag[:2], word, story)
     return story
+
+def test_indexReplace():
+    assert "i want to go"[3:5] == "an"
+    assert indexReplace([3,5], 'jake', "i want to go") == "i wjaket to go"
+
+def test_find_tags():
+    assert find_tag("<NOUN>") == [0, 6, 'NOUN']
+    s = "hello my <NOUN>"
+    assert find_tag(s) == [9, 15, 'NOUN']
+    assert s[find_tag(s)[0] : find_tag(s)[1]] == "<NOUN>"
+    assert find_tag("helllo my <FACE>") == False
+    assert find_tag("hello my /<NOUN/>") == False
+
+def test_fillBlanks():
+    assert fillBlanks("<NOUN>") in nouns
+    print fillBlanks("The <ADJECTIVE> <NOUN> <VERB> <ADVERB> upside down.")
