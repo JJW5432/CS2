07992666d5567c3f020e43c357a9aea697c53d32
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-05-09 08:41:41 -0400

    foo

--- gutenberg/gutenberg.py
+++ gutenberg/gutenberg.py
@@ -1,18 +1,12 @@
-file = open('./story.txt', 'r')
-ptext = file.read()
-file.close()
+FILE = './story.txt'
+
+def main(file):
+    file = open(file, 'r')
+    ptext = file.read()
+    file.close()
 #print text
 
 from string import punctuation
-
-#def jake_strip(word, chars):
-#    while word[0] in chars or word[-1] in chars:
-#        if word[0] in chars:
-#            word = word[1:]
-#        elif word[-1] in chars:
-#            word = word[:-1]
-#    return word
-
 
 def word_count(text):
     words = text.split(' ')
@@ -25,3 +19,15 @@ def word_count(text):
         else:
             buckets[word] = 1
     return buckets
+
+def cutoff(dict, cutoff):
+    new_dict = {}
+    while len(new_dict) < cutoff:
+        highest = max(dict.values())
+        current_len = len(new_dict)
+        index = 0
+        while len(new_dict) == current_len and index < len(dict):
+            key = dict.keys()[index]
+            if dict[key] == highest:
+                new_dict[key] == highest
+                
